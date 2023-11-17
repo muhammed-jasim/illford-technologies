bb246b12f0af72cfa822ae75da1060443c98e948
Harikrishnancodes <harih6176@gmail.com>
2023-11-17 14:14:47 +0530

    about section

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+
+import "../about/about.css"
+import RootLayout from "../layout"
+
+
+const About = () => {
+  return (
+    <RootLayout >
+    <div className="about-container">
+      
+        <h1>this is about</h1>
+        </div>
+        </RootLayout>
+  )
+}
+
+export default About
